eac504492a38b4bf4a549e8ac2010498dca1cfab
Calista Pemberton <calista@Calistas-MacBook-Air.local>
2025-07-24 13:36:58 +0800

    dear god

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,6 +1,3 @@
-import { Github } from "@medusajs/icons"
-import { Button, Heading } from "@medusajs/ui"
-
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
